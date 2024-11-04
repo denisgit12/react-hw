9b3dbdbc261bbb5dd98d7d08f427c54c9c3b9350
denisgit12 <yaholnykd@gmail.com>
2024-11-04 10:45:39 +0100

    hw-6

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -32,6 +32,7 @@ const Products = () => {
                         } else {
                             navigate('/login')
                         }
+
                     }
                 }
             })
